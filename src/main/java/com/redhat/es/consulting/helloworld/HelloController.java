43187ae1fe6d25e021b1c75fd5c032fdab769561
Alvaro Lopez Medina <alopezme@redhat.com>
2019-12-12 23:47:07 +0100

    Added Config File

--- src/main/java/com/redhat/es/consulting/helloworld/HelloController.java
+++ src/main/java/com/redhat/es/consulting/helloworld/HelloController.java
@@ -1,5 +1,6 @@
 package com.redhat.es.consulting.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	@Value("${controller.greeting}")
+	private String greeting;
+	
 	@RequestMapping("/hola")
 	  public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
 		String hostname = System.getenv().getOrDefault("HOSTNAME", "Unknown");
-        return String.format("Hola %s desde %s ", name, hostname);
+        return String.format("%s %s desde %s ", greeting, name, hostname) + System.lineSeparator();
 	  }
 }
